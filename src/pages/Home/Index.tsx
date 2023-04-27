ae4035481dca1a7f0a7b981a23edbad107d02e14
João Pedro <joao.pedro.franchini.4@gmail.com>
2023-04-27 15:33:28 -0300

    Estilzando o form

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,3 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  HomeContainer,
+  FormContainer,
+  CountdownContainer,
+  Separator,
+  StartCountdownButton,
+} from './Styles'
 export function Home() {
-  return <div></div>
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label>Durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
 }
